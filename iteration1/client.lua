9f5726ae90b53598571d34f794a5b658fedcc555
Corey Matyas <corey@coreymatyas.com>
2014-05-28 13:42:10 -0400

    Rearrange folder structure.

--- iteration1/client.lua
+++
@@ -1,8 +0,0 @@
-modem = peripheral.wrap("back") -- location of modem
-modem.open(1) -- testing broadcast channel
-
-while true do
-	modem.transmit(1, 1, "foo") -- send "foo" on channel 1
-	print("Sent 'foo'")
-	sleep(5)
-end
--- iteration1/receiver.lua
+++
@@ -1,7 +0,0 @@
-modem = peripheral.wrap("front")
-modem.open(1)
-
-while true do
-	local event, recv, sChan, rChan, message, distance = os.pullEvent("modem_message")
-	print(message)
-end
